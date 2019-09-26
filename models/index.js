657d88ccaaecbdc381c638455a4b42df0c1018c7
Daniel <djumelle@oldwestbury.edu>
2019-09-26 18:20:00 -0400

    added more functionality

--- models/index.js
+++ models/index.js
@@ -3,27 +3,38 @@ const db = new Sequelize('postgres://localhost:5432/wikistack', {
     logging: false
 });
 
-const Page = db.define('page',{
-    title: {type : Sequelize.STRING,
-            allowNull:false},
-    slug: {type : Sequelize.STRING,
-           allowNull:false},
-    content:{type:Sequelize.TEXT,
-             allowNull:false},
-    status: {type:Sequelize.ENUM('open','closed')}
-})
-Page.beforeValidate(PageInstance =>{
-PageInstance.slug = PageInstance.title.replace(/\s+/g, '_').replace(/\W/g, '')
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: { type: Sequelize.ENUM('open', 'closed') }
 })
 
-const User = db.define('user',{
-    name:{type:Sequelize.STRING,
-          allowNull:false},
-    email:{type:Sequelize.STRING,
-           allowNull:false,
+Page.beforeValidate(PageInstance => {
+    PageInstance.slug = PageInstance.title.replace(/\s+/g, '_').replace(/\W/g, '')
+})
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
         validate: {
-            isEmail:true
-        }}
+            isEmail: true
+        }
+    }
 })
 
 module.exports = {
--- routes/wiki.js
+++ routes/wiki.js
@@ -3,20 +3,19 @@ const router = express.Router()
 const addPage = require('../views/addPage')
 const mainPage = require('../views/main')
 const { Page } = require("../models");
+const wikiPage = require("../views/wikipage")
 
 router.post('/', async (req, res, next) => {
-
   const newPage = new Page({
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(newPage.title)
+
   try {
     await newPage.save();
     res.redirect('/');
   } catch (error) { next(error) }
 });
-
 
 router.get('/', (req, res, next) => {
   res.send(mainPage())
@@ -28,6 +27,17 @@ router.post('/', (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
   res.send(addPage())
+});
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const currentPage = await Page.findOne({
+      where: { slug: req.params.slug }
+    });
+    // console.log(req.params.slug)
+    // console.log(currentPage)
+    res.json(currentPage)
+  } catch (error) { next(error) }
 });
 
 module.exports = router
